5f8ed3bc26572d978985e73a18d28f1d98b42cef
FrogMakesSkripts <cryomakesskripts@gmail.com>
2026-03-11 15:33:32 -0400

    Initial commit

---
+++ src/main/java/net/frogmakesskripts/froggysaddons/FroggysAddonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.frogmakesskripts.froggysaddons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FroggysAddonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
