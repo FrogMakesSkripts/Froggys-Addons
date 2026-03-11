13c29cb5fe5269472d1832fa4188e85cc73f4bc9
FrogMakesSkripts <cryomakesskripts@gmail.com>
2026-03-11 16:02:01 -0400

    Added client initializer & Updated information

---
+++ src/main/java/net/frogmakesskripts/froggysaddons/FroggysAddonsClient.java
@@ -0,0 +1,10 @@
+package net.frogmakesskripts.froggysaddons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FroggysAddonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
