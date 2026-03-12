0f8c7685e852aa0a27e33eb9a0147773b284a9c7
FrogMakesSkripts <cryomakesskripts@gmail.com>
2026-03-12 14:00:02 -0400

    Fixed asset directory & Added custom blocks

--- src/main/java/net/frogmakesskripts/froggysaddons/FroggysAddons.java
+++ src/main/java/net/frogmakesskripts/froggysaddons/FroggysAddons.java
@@ -2,15 +2,18 @@ package net.frogmakesskripts.froggysaddons;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.frogmakesskripts.froggysaddons.block.ModBlocks;
+import net.frogmakesskripts.froggysaddons.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FroggysAddons implements ModInitializer {
-	public static final String MOD_ID = "froggys-addons";
+	public static final String MOD_ID = "froggysaddons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
